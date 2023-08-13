6808029a64b46593e34ace374809b6bcba2b7051
赵某某 <1289890735@qq.com>
2023-08-13 14:30:06 +0800

    1

--- src/components/lefts.vue
+++ src/components/lefts.vue
@@ -173,9 +173,6 @@ export default {
   props: {
     msg: String,
   },
-  beforeUpdate() {
-    this.arr = JSON.parse(localStorage.getItem("arr11"));
-  },
 };
 </script>
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
